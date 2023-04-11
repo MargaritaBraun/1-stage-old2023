00a9e2de61d429aa9d13a0244218b9f3f9fde8be
MargaritaBraun <119820661+MargaritaBraun@users.noreply.github.com>
2023-04-11 17:27:05 +0300

    fix: style modal window

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,21 @@ module.exports = {
       filename: "main.js"
     },
 
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
+      }
+  ],
+
     watchOptions: {
         aggregateTimeout: 600,
       },
+
   }
