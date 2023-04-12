45fbd3540902289d087ee30f8de7c5ac4fe84d39
MargaritaBraun <119820661+MargaritaBraun@users.noreply.github.com>
2023-04-12 23:26:49 +0300

    fix: burger menu

--- src/js/Hamburger.js
+++ src/js/Hamburger.js
@@ -1,12 +1,13 @@
 const hamburger = document.querySelector('.burger__container');
 const navMenu = document.querySelector('.nav-menu');
-
 const body = document.body;
+const overlei = document.querySelector('.overlei');
 
 if (navMenu && hamburger ) {
-    hamburger.addEventListener('click', () => {
+    hamburger.addEventListener('click', (e) => {
         navMenu.classList.toggle('active')
         hamburger.classList.toggle('active')
+        overlei.classList.toggle('lock')
         body.classList.toggle('lock')
     })
 
@@ -14,8 +15,42 @@ if (navMenu && hamburger ) {
         link.addEventListener('click', () => {
             navMenu.classList.remove('active')
             hamburger.classList.remove('active')
-            body.classList.remove('lock')
+            overlei.classList.remove('lock')
+            body.classList.toggle('lock')
         })
     })
 }
 
+  navMenu.addEventListener('click', e => {
+        if (e.target.classList.contains('item-menu')) {
+            navMenu.classList.remove('active')
+            hamburger.classList.remove('active')
+            overlei.classList.remove('lock')
+            body.classList.remove('lock')
+        } else if (!e.target.classList.contains('item-menu')) {
+            navMenu.classList.remove('active')
+            hamburger.classList.remove('active')
+            overlei.classList.remove('lock')
+            body.classList.remove('lock')
+        }
+    })
+
+
+
+const anchors = document.querySelectorAll('item-menu');
+
+anchors.forEach(anchor => {
+   anchor.addEventListener('click', event => {
+    event.preventDefault();
+   })
+})
+
+
+
+    navMenu.addEventListener('moseup', () => {
+        if (navMenu.has(e.target).length === 0){
+            navMenu.classList.remove('active')
+            hamburger.classList.remove('active')
+            overlei.classList.remove('lock')
+        }
+    });
